eff0416a1212804919c05104a0dae7c842f78240
SvenMayCry <svenmaycry@gmail.com>
2024-11-26 10:48:01 +0300

    Добавит секцию с "Типами резки"

--- js/blocks/modal-penguin.js
+++ js/blocks/modal-penguin.js
@@ -1,12 +1,12 @@
 export const initModalPenguin = () => {
-  
-  const penguinModal = document.querySelector('.js-penguin');
+
+  const penguinModal = document.querySelector('.js-penguin-right');
   const penguinModalCloseBtn = document.querySelector('.js-penguin__close-btn');
 
   if (penguinModal) {
     document.addEventListener("DOMContentLoaded", function () {
       setTimeout(function () {
-        penguinModal.style.right = "20px";
+        penguinModal.style.right = "-100px";
       }, 4000);
 
       penguinModalCloseBtn.addEventListener("click", function () {
